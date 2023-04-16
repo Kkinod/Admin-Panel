49cb0ac8e01d495c9dbdca2e15dbf991ce28b658
Kamil <kamil.p@tutanota.com>
2023-04-16 19:32:39 +0200

    add data, models, controllers and routes folders

--- server/index.tsx
+++ server/index.tsx
@@ -1,24 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan";
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import clientRoutes from './routes/client'
+import generalRoutes from './routes/general'
+import managementRoutes from './routes/management'
+import salesRoutes from './routes/sales'
 
 /* CONFIGURATION */
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 /* ROUTES */
-app.use("/client", clientRoutes);
-app.use("/general", generalRoutes);
-app.use("/management", managementRoutes);
-app.use("/sales", salesRoutes);
+app.use('/client', clientRoutes)
+app.use('/general', generalRoutes)
+app.use('/management', managementRoutes)
+app.use('/sales', salesRoutes)
