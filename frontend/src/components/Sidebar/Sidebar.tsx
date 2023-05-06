24c179216302ca1f44d291ee8fe76aa91bfd1cf4
Kamil <kamil.p@tutanota.com>
2023-05-06 12:02:47 +0200

    feat: add Sidebar base
    add Sidebar to Layout, create SideBar base

---
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import {
+  AdminPanelSettingsOutlined,
+  CalendarMonthOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  PieChartOutline,
+  PointOfSaleOutlined,
+  PublicOutlined,
+  ReceiptLongOutlined,
+  ShoppingCartOutlined,
+  SettingsOutlined,
+  TodayOutlined,
+  TrendingUpOutlined,
+} from '@mui/icons-material';
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
