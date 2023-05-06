cfead458c71ae6f99975b9e497aca2f3d52b9e04
Kamil <kamil.p@tutanota.com>
2023-05-06 17:12:44 +0200

    feat: Sidebar logic
    add displaying the section, which is a list of items contained in 'navItems'. Adding styles to the active section

--- frontend/src/components/Sidebar/Sidebar.styles.tsx
+++ frontend/src/components/Sidebar/Sidebar.styles.tsx
@@ -1,8 +1,13 @@
 import { Box } from '@mui/material';
 import { styled } from '@mui/system';
+import { flexBetween } from '../../assets/styles/mixins.styles';
 
-export const Container = styled(Box)(({ isNonMobile }: ContainerProps) => ({
-  display: isNonMobile ? 'flex' : 'block',
-  width: '100%',
-  height: '100%',
-}));
+// export const Container = styled(Box)(({ isNonMobile }: ContainerProps) => ({
+//   display: isNonMobile ? 'flex' : 'block',
+//   width: '100%',
+//   height: '100%',
+// }));
+
+export const FlexBetween = styled(Box)({
+  ...flexBetween,
+});
--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
   Box,
   Divider,
-  Drawer,
   IconButton,
   List,
   ListItem,
@@ -13,6 +12,7 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
+import MuiDrawer from '@mui/material/Drawer';
 import {
   AdminPanelSettingsOutlined,
   CalendarMonthOutlined,
@@ -27,8 +27,69 @@ import {
   SettingsOutlined,
   TodayOutlined,
   TrendingUpOutlined,
+  Groups2Outlined,
+  PieChartOutlined,
 } from '@mui/icons-material';
+import { FlexBetween } from './Sidebar.styles';
 
+const navItems = [
+  {
+    text: 'Dashboard',
+    icon: <HomeOutlined />,
+  },
+  {
+    text: 'Client Facing',
+    icon: null,
+  },
+  {
+    text: 'Products',
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: 'Customers',
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: 'Transactions',
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: 'Geography',
+    icon: <PublicOutlined />,
+  },
+  {
+    text: 'Sales',
+    icon: null,
+  },
+  {
+    text: 'Overview',
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: 'Daily',
+    icon: <TodayOutlined />,
+  },
+  {
+    text: 'Monthly',
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: 'Breakdown',
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: 'Management',
+    icon: null,
+  },
+  {
+    text: 'Admin',
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: 'Performance',
+    icon: <TrendingUpOutlined />,
+  },
+];
 interface ISidebar {
   drawerWidth: string;
   isNonMobile: boolean;
@@ -44,15 +105,19 @@ const Sidebar = ({
   setIsSidebarOpen,
 }: ISidebar) => {
   const { pathname } = useLocation();
+  const [active, setActive] = useState('');
   const navigate = useNavigate();
   const theme = useTheme();
 
-  const active = useMemo(() => pathname.substring(1), [pathname]);
+  //   const active = useMemo(() => pathname.substring(1), [pathname]);
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
 
   return (
     <Box component="nav">
       {isSidebarOpen && (
-        <Drawer
+        <MuiDrawer
           open={isSidebarOpen}
           onClose={() => setIsSidebarOpen(false)}
           variant="persistent"
@@ -66,7 +131,65 @@ const Sidebar = ({
               width: drawerWidth,
             },
           }}
-        />
+        >
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    Mr. WOODENFLOOR
+                  </Typography>
+                </Box>
+                {!isNonMobile && (
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            </Box>
+            <List>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: '2.25rem 0 1rem 3rem' }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLowerCase();
+
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText ? 'red' : 'transparent',
+                        color: active === lcText ? 'green' : 'blue',
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          ml: '2rem',
+                          color: active === lcText ? 'green' : 'yellow',
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: 'auto' }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+        </MuiDrawer>
       )}
     </Box>
   );
