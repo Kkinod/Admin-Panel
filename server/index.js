42f8a87a2512737c4d778575f4b38153a4f3a76a
Kamil <kamil.p@tutanota.com>
2023-05-13 14:30:12 +0200

    feat: backend configuration for dataUser
    add mongoose schema for dataUser, add router and controller for dataUser

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js'
 import managementRoutes from './routes/management.js'
 import salesRoutes from './routes/sales.js'
 
+// data imports
+import User from './models/User.js' 
+import { dataUser } from './data/data.js'
+
 /* CONFIGURATION */
 dotenv.config()
 const app = express()
