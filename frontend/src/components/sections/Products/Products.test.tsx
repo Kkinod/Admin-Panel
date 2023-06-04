7279fef2d2254c0515133a6be4948fede1eeb841
Kamil <kamil.p@tutanota.com>
2023-06-04 20:43:03 +0200

    tests: add test for render product card

--- frontend/src/components/sections/Products/Products.test.tsx
+++ frontend/src/components/sections/Products/Products.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import '@testing-library/jest-dom/extend-expect';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { screen } from '@testing-library/react';
 import Products from './Products';
-
 import { mockedProductData } from '../../../testUtils/mockedData';
 import { renderWithProviders } from '../../../testUtils/renderWithProviders';
 
@@ -25,11 +24,23 @@ describe('Products', () => {
     );
   });
 
-  it('renders header', () => {
+  it('should renders header', () => {
     const headerTitle = screen.getByText('PRODUCTS');
     const headerSubtitle = screen.getByText('See your list of products.');
 
     expect(headerTitle).toBeInTheDocument();
     expect(headerSubtitle).toBeInTheDocument();
+  });
+
+  it('should renders product card', () => {
+    const productName = screen.getByText(mockedProductData.name);
+    const productDescription = screen.getByText(mockedProductData.description);
+    const productPrice = screen.getByText(
+      `$${mockedProductData.price.toFixed(2)}`
+    );
+
+    expect(productName).toBeInTheDocument();
+    expect(productDescription).toBeInTheDocument();
+    expect(productPrice).toBeInTheDocument();
   });
 });
