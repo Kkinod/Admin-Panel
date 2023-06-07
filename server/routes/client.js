82d5146a42c66cdce12fbfda33d8a529567b53e5
Kamil <kamil.p@tutanota.com>
2023-06-07 13:32:20 +0200

    feat: add backend for transactions
    implementing server side pagination support

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getProducts } from '../controllers/client.js'
+import { getProducts, getTransactions } from '../controllers/client.js'
 
 const router = express.Router()
 
 router.get('/products', getProducts)
+router.get('/transactions', getTransactions)
 
 export default router
