87eca89606de7d7b4a7d49283139f25790eef7f1
Kamil <kamil.p@tutanota.com>
2023-06-12 12:59:03 +0200

    feat: changing the columns array as a reusable component

--- frontend/src/components/sections/Users/Users.tsx
+++ frontend/src/components/sections/Users/Users.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
 import {
   DataGrid,
-  GridCellParams,
-  GridColDef,
   GridColumnVisibilityModel,
   GridRowSelectionModel,
   GridToolbarColumnsButton,
@@ -11,13 +9,13 @@ import {
   GridToolbarDensitySelector,
   GridToolbarExport,
   GridToolbarFilterButton,
-  plPL,
 } from '@mui/x-data-grid';
 import Button from '@mui/material/Button';
-import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 import { useGetUsersQuery } from '../../../features/api';
 import Header from '../../Header/Header';
+import { generateColumns } from '../../../utils/columns/columns';
 import './Users.css';
+import { StyledBoxContainer } from './Users.styles';
 
 interface IUsers {
   isMaxWidth600px: boolean;
@@ -29,8 +27,6 @@ const Users = ({ isMaxWidth600px, isXsDown1025 }: IUsers) => {
   const { data, isLoading } = useGetUsersQuery(null);
   const [selectionModelState, setSelectionModelState] =
     React.useState<GridRowSelectionModel>([]);
-
-  console.log(data);
 
   const [columnVisibility, setColumnVisibility] =
     React.useState<GridColumnVisibilityModel>({
@@ -44,73 +40,10 @@ const Users = ({ isMaxWidth600px, isXsDown1025 }: IUsers) => {
     console.log(selectionModelState);
   };
 
-  const columns: GridColDef[] = [
-    {
-      field: '_id',
-      headerName: 'ID',
-      minWidth: 190,
-      flex: 1,
-    },
-    {
-      field: 'name',
-      headerName: 'Name',
-      minWidth: 80,
-      flex: 1,
-    },
-    {
-      field: 'email',
-      headerName: 'Email',
-      minWidth: 180,
-      flex: 1,
-    },
-    {
-      field: 'phoneNumber',
-      headerName: 'Phone number',
-      minWidth: 100,
-      flex: 1,
-      renderCell: (params: GridCellParams<any, any, string>) => {
-        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, '($1)$2-$3');
-      },
-    },
-    {
-      field: 'country',
-      headerName: 'Country',
-      flex: 0.4,
-    },
-    {
-      field: 'role',
-      headerName: 'Role',
-      width: 120,
-      renderCell: ({ row: { role } }) => {
-        return (
-          <Box
-            width="85%"
-            m="0 auto"
-            p="5px"
-            display="flex"
-            justifyContent="center"
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-            }}
-            borderRadius="4px"
-          >
-            <LockOpenOutlinedIcon />
-            <Typography
-              color={theme.palette.secondary.main}
-              sx={{
-                ml: '5px',
-              }}
-            >
-              {role}
-            </Typography>
-          </Box>
-        );
-      },
-    },
-  ];
+  const columns = generateColumns({ includeRoleColumn: false });
 
   return (
-    <Box className="adminUsers__container">
+    <StyledBoxContainer>
       <Header title="Users" subtitle="Subtitle" />
       <Box
         mt="1rem"
@@ -161,21 +94,9 @@ const Users = ({ isMaxWidth600px, isXsDown1025 }: IUsers) => {
           components={{
             Toolbar: () => (
               <GridToolbarContainer>
-                <GridToolbarColumnsButton
-                  nonce={undefined}
-                  onResize={undefined}
-                  onResizeCapture={undefined}
-                />
-                <GridToolbarFilterButton
-                  nonce={undefined}
-                  onResize={undefined}
-                  onResizeCapture={undefined}
-                />
-                <GridToolbarDensitySelector
-                  nonce={undefined}
-                  onResize={undefined}
-                  onResizeCapture={undefined}
-                />
+                <GridToolbarColumnsButton />
+                <GridToolbarFilterButton />
+                <GridToolbarDensitySelector />
                 <GridToolbarExport />
                 {selectionModelState.length > 0 && (
                   <Button
@@ -191,7 +112,7 @@ const Users = ({ isMaxWidth600px, isXsDown1025 }: IUsers) => {
           }}
         />
       </Box>
-    </Box>
+    </StyledBoxContainer>
   );
 };
 
