b4f0eb256e15c65b7bd8cfc249bd48e5a6d512f0
Kamil <kamil.p@tutanota.com>
2023-06-21 13:19:30 +0200

    feat: add a 'labels' file to transfer all texts to it

--- frontend/src/utils/columns/actionColumn/actionColumn.styles.tsx
+++ frontend/src/utils/columns/actionColumn/actionColumn.styles.tsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 export const StyledBoxWrapper = styled(Box)({
   display: 'flex',
   alignItems: 'center',
-  gap: '0.8rem',
+  gap: '0.3rem',
 });
 
 export const StyledLink = styled(Link)({
   padding: '2px 5px',
-  border: '1px dotted rgba(0, 0, 139, 0.596)',
+  border: '1px solid rgba(0, 0, 139, 0.596)',
   borderRadius: '5px',
   color: 'darkblue',
   fontWeight: 'bold',
