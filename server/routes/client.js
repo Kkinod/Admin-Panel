a9b3c6c648080aa69d7ca353a22a3843fa890242
Kamil <kamil.p@tutanota.com>
2023-06-27 13:19:20 +0200

    feat: add backend for Geography

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,10 +1,16 @@
-import express from 'express'
-import { getProducts, getUsers, getTransactions } from '../controllers/client.js'
+import express from "express";
+import {
+  getProducts,
+  getUsers,
+  getTransactions,
+  getGeography,
+} from "../controllers/client.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/products', getProducts)
-router.get('/transactions', getTransactions)
-router.get('/users', getUsers)
+router.get("/products", getProducts);
+router.get("/transactions", getTransactions);
+router.get("/users", getUsers);
+router.get("/geography", getGeography);
 
-export default router
+export default router;
