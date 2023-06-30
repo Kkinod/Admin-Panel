10272db5daf89a413a7f99548d2240e405563c0b
Kamil <kamil.p@tutanota.com>
2023-06-30 09:34:56 +0200

    refactor: text to labels in the ProductCard component

--- frontend/src/components/sections/Products/ProductCard/ProductCard.styles.tsx
+++ frontend/src/components/sections/Products/ProductCard/ProductCard.styles.tsx
@@ -1,19 +1,23 @@
-import { Card, Collapse, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Card, CardContent, Collapse, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const CardContentStyled = styled(CardContent)({
+  height: "14rem",
+});
 
 export const CardStyled = styled(Card)(({ theme }) => ({
-  backgroundImage: 'none',
+  backgroundImage: "none",
   backgroundColor: theme.palette.secondary.main,
-  borderRadius: '0.55rem',
+  borderRadius: "0.55rem",
 }));
 
 export const TypographyCategoryStyled = styled(Typography)(({ theme }) => ({
-  fontSize: '1rem',
+  fontSize: "1rem",
   color: theme.palette.secondary.dark,
 }));
 
 export const TypographyPriceStyled = styled(Typography)(({ theme }) => ({
-  marginBottom: '1.5rem',
+  marginBottom: "1.5rem",
   color: theme.palette.primary.main,
 }));
 
